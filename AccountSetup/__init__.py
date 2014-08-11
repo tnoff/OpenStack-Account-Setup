1d08f30655b471423018211c1d2bd464e24a3b59
tylernorth <tylernorth18@gmail.com>
2014-08-10 21:33:38 -0700

    Add secuirity groups

--- AccountSetup/__init__.py
+++ AccountSetup/__init__.py
@@ -2,9 +2,15 @@ from cinderclient.v1 import client as cinder_v1
 from keystoneclient.v2_0 import client as key_v2
 from keystoneclient.openstack.common.apiclient import exceptions as keystone_exceptions
 from novaclient.v1_1 import client as nova_v1
+from novaclient import exceptions as nova_exceptions
 
 class AccountSetup(object):
     def __init__(self, username, password, tenant_name, auth_url):
+        self.os_username = username
+        self.os_password = password
+        self.os_tenant_name = tenant_name
+        self.os_auth_url = auth_url
+
         self.keystone = key_v2.Client(username=username,
                                       password=password,
                                       tenant_name=tenant_name,
@@ -36,6 +42,12 @@ class AccountSetup(object):
                 return tenant
         return None
 
+    def __find_sec_group(self, nova, name):
+        for group in nova.security_groups.list():
+            if group.name == name:
+                return group.id
+        return None
+
     def create_user(self, **args):
         try:
             user = self.keystone.users.create(**args)
@@ -48,10 +60,15 @@ class AccountSetup(object):
         role = self.__find_role(args.pop('role', None))
         args['tenant_name'] = args.pop('name', None)
         try:
-            project = self.keystone.tenants.create(**args)
+            self.keystone.tenants.create(**args)
+            project = self.__find_project(args['tenant_name'] or  None)
         except keystone_exceptions.Conflict:
-            project = self.__find_project(args.pop('name', None))
-        project.add_user(user.id, role.id)
+            project = self.__find_project(args['tenant_name'] or None)
+        try:
+            project.add_user(user.id, role.id)
+        except keystone_exceptions.Conflict:
+            # Role already exists
+            pass
 
     def set_nova_quota(self, **args):
         project = self.__find_project(args.pop('tenant_name', None))
@@ -61,12 +78,34 @@ class AccountSetup(object):
         project = self.__find_project(args.pop('tenant_name', None))
         self.cinder.quotas.update(project.id, **args)
 
+    def create_security_group(self, user, user_password, **args):
+        tenant_name = args.pop('tenant_name', None)
+        rules = args.pop('rules', None)
+        nova = nova_v1.Client(user.name,
+                              user_password,
+                              tenant_name,
+                              self.os_auth_url)
+        try:
+            group = nova.security_groups.create(**args)
+        except nova_exceptions.BadRequest:
+            # Group already exists
+            group = self.__find_sec_group(nova, args.pop('name', None))
+        for rule in rules:
+            try:
+                nova.security_group_rules.create(group, **rule)
+            except nova_exceptions.BadRequest:
+                # Rule already exits
+                pass
+
     def setup_config(self, config):
         print config
         user = self.create_user(**config['user'])
+        user_password = config['user']['password']
         for project in config['projects']:
             self.create_project(user, **project)
         for quota in config['nova_quotas']:
             self.set_nova_quota(**quota)
         for quota in config['cinder_quotas']:
             self.set_cinder_quota(**quota)
+        for group in config['security_groups']:
+            self.create_security_group(user, user_password, **group)
