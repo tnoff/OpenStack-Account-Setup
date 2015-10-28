e4fa0f84ecca557cbddecd4b6f4a3b5b8c87f037
tylernorth <tylernorth18@gmail.com>
2015-10-28 12:46:05 -0700

    remove versions from openstack args

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setuptools.setup(
     author_email='tylernorth18@gmail.com',
     description='OpenStack Account Setup Script',
     install_requires=[
-        'python-cinderclient >= 1.0.9',
-        'python-glanceclient >= 0.13.1',
-        'python-keystoneclient >= 0.10.1',
-        'python-neutronclient>=2.3.11',
-        'python-novaclient >= 2.18.1',
+        'python-cinderclient',
+        'python-glanceclient',
+        'python-keystoneclient',
+        'python-neutronclient',
+        'python-novaclient',
 
         'nose >= 1.3.7',
         'pycrypto >= 2.6.1',
